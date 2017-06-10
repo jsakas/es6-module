776b7fb20f12f1355116ee070486125c933a413d
Jon Sakas <jon.sakas@beatport.com>
2017-06-10 10:02:02 -0600

    Generate documentation

--- dist/common/index.js
+++ dist/common/index.js
@@ -4,6 +4,13 @@ var _createClass = function () { function defineProperties(target, props) { for 
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
+/**
+ * The `Math` class provides some simple math functions.
+ * @example
+ * let math = new Math();
+ * let sum = math.sum(3, 5);
+ * console.log(sum);
+ */
 var Math = function () {
   function Math() {
     _classCallCheck(this, Math);
@@ -11,19 +18,46 @@ var Math = function () {
 
   _createClass(Math, [{
     key: "add",
+
+    /**
+     * @param {number} x
+     * @param {number} y
+     * @return {number} sum of x and y
+     */
     value: function add(x, y) {
       return x + y;
     }
+
+    /**
+     * @param {number} x
+     * @param {number} y
+     * @return {number} difference of x and y
+     */
+
   }, {
     key: "subtract",
     value: function subtract(x, y) {
       return x - y;
     }
+
+    /**
+     * @param {number} x
+     * @param {number} y
+     * @return {number} product of x and y
+     */
+
   }, {
     key: "multiply",
     value: function multiply(x, y) {
       return x * y;
     }
+
+    /**
+     * @param {number} x
+     * @param {number} y
+     * @return {number} quotient of x and y
+     */
+
   }, {
     key: "divide",
     value: function divide(x, y) {
--- src/index.js
+++ src/index.js
@@ -1,16 +1,43 @@
+/**
+ * The `Math` class provides some simple math functions.
+ * @example
+ * let math = new Math();
+ * let sum = math.sum(3, 5);
+ * console.log(sum);
+ */
 class Math {
+  /**
+   * @param {number} x
+   * @param {number} y
+   * @return {number} sum of x and y
+   */
   add (x, y) {
     return x + y
   }
 
+  /**
+   * @param {number} x
+   * @param {number} y
+   * @return {number} difference of x and y
+   */
   subtract (x, y) {
     return x - y
   }
 
+  /**
+   * @param {number} x
+   * @param {number} y
+   * @return {number} product of x and y
+   */
   multiply (x, y) {
     return x * y
   }
 
+  /**
+   * @param {number} x
+   * @param {number} y
+   * @return {number} quotient of x and y
+   */
   divide (x, y) {
     return x / y
   }
